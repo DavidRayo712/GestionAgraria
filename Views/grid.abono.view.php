510b459b5ac8365078ae0adf6a4aedf977ebbd9f
David Torres <70404634+DavidRayo712@users.noreply.github.com>
2022-05-04 20:05:31 +0200

    Update grid.abono.view

--- Views/grid.abono.view.php
+++ Views/grid.abono.view.php
@@ -1,24 +1,4 @@
 <div class="container text-center">
-    <div class="find-form g-3 mt-5">
-        <div class="">
-            <span class="title-find p-2 text-white rounded">
-                Periodo
-            </span>
-        </div>
-        <div class="mt-4 mb-4">
-            <span class="label-from-to">
-                Desde <span class="subtitle-find font-weight-bold">
-                <?php
-                    echo  $_POST['from'] ;
-                ?>
-                </span> hasta <span class="subtitle-find font-weight-bold">
-                <?php
-                    echo  $_POST['to'] ;
-                ?>
-                </span>
-            </span>
-        </div>
-    </div>
 
 <table class="table table-striped table-abono">
 <thead>
@@ -28,7 +8,6 @@
       <th scope="col"><div class="column-abono">Producto</div></th>
       <th scope="col"><div class="column-abono">Cantidad</div></th>
       <th scope="col"><div class="column-abono">Precio</div></th>
-      <th scope="col"><div class="column-abono">Importe</div></th>
     </tr>
   </thead>
   <tbody>
@@ -39,8 +18,7 @@
         echo '<td>'.$abono['proveedor'].'</td>';
         echo '<td>'.$abono['producto'].'</td>';
         echo '<td>'.$abono['cantidad'].' Kg</td>';
-        echo '<td>'.$abono['precio'].' € / kg</td>';
-        echo '<td>'.$abono['importe'].' €</td>';
+        echo '<td>'.$abono['precio'].' €</td>';
         echo '</tr>';
         }
     ?>
